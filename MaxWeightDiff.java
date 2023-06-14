9e192b05fa734707661321fb9e8031a12cbf0a54
Satyam Shrivastava <117986866+satyam380@users.noreply.github.com>
2023-06-14 18:26:29 +0530

    Add files via upload
    Solution of "Maximum Weight Difference" for Plus91

---
+++ MaxWeightDiff.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class MaxWeightDiff
+{
+	public static void main (String[] args)
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] w = new int[N];
+
+		int split1 = 0;
+		int split2 = 0;
+		for(int i = 0; i < N; i++)
+		{
+			w[i] = sc.nextInt();
+		}
+		Arrays.sort(w);
+
+		for(int i = 0; i < K; i++)
+		{
+			split1 += w[i];
+		}      
+		for(int i = K; i < N; i++)
+		{
+			split2 += w[i];
+		}
+		long h = split2-split1;
+		System.out.println(h);
+		sc.close();
+	}
+}
